4243c51da4aee85c918eae2c5f53d4ef53557654
Nonanorexic <saxenaaakriti96@gmail.com>
2023-02-20 23:37:47 +0530

    registration done

--- server/router/adminRoute.js
+++ server/router/adminRoute.js
@@ -6,7 +6,9 @@ const bcrypt = require("bcryptjs");
 require("../db/database");
 router.use(express.static("../client/src/"));
 const Admin = require("../dbmodel/admin");
+const Institute = require("../dbmodel/instituteSchema");
 
+//adminlogin
 router.post("/admin-login", async (req, res) => {
   const { user } = req.body;
   if (user === "admin") {
@@ -41,5 +43,77 @@ router.post("/admin-login", async (req, res) => {
     }
   }
 });
+
+//insitutelogin
+/*router.post("/admin-login", async (req, res) => {
+  const { user } = req.body;
+  if (user === "institute") {
+    try {
+      let token;
+      const { email, password } = req.body;
+      if (!email || !password) {
+        return res
+          .status(400)
+          .json({ error: "Field not filled properly in login page " });
+      }
+
+      const adminLogin = await Admin.findOne({ email: email });
+
+      if (adminLogin) {
+        token = await adminLogin.generateAuthToken();
+        res.cookie("jwtoken", token, {
+          expires: new Date(Date.now() + 25892000000),
+          httpOnly: true,
+        });
+
+        if (password != adminLogin.password) {
+          res.status(400).json({ error: "Incorrect Password" });
+        } else {
+          res.status(200).json({ message: "Admin login successfully ✌🏼" });
+        }
+      } else {
+        res.status(400).json({ error: "Invalid Credientials" });
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+});*/
+
+//coordinatorlogin
+/*router.post("/admin-login", async (req, res) => {
+  const { user } = req.body;
+  if (user === "institute") {
+    try {
+      let token;
+      const { email, password } = req.body;
+      if (!email || !password) {
+        return res
+          .status(400)
+          .json({ error: "Field not filled properly in login page " });
+      }
+
+      const adminLogin = await Admin.findOne({ email: email });
+
+      if (adminLogin) {
+        token = await adminLogin.generateAuthToken();
+        res.cookie("jwtoken", token, {
+          expires: new Date(Date.now() + 25892000000),
+          httpOnly: true,
+        });
+
+        if (password != adminLogin.password) {
+          res.status(400).json({ error: "Incorrect Password" });
+        } else {
+          res.status(200).json({ message: "Admin login successfully ✌🏼" });
+        }
+      } else {
+        res.status(400).json({ error: "Invalid Credientials" });
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+});*/ 
 
 module.exports = router;
