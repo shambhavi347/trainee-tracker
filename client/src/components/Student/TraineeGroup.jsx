7f9d597d1da360ed6c1635d567dbc0c4929a466c
riddle17 <ritu171197@gmail.com>
2023-02-22 01:27:05 +0530

    trainee group

--- client/src/components/Student/TraineeGroup.jsx
+++ client/src/components/Student/TraineeGroup.jsx
@@ -4,50 +4,51 @@ import "../../CSS/Trainee/TraineeGroup.css";
 const TraineeGroup = () => {
   const [mentor, setMentor] = useState({ name: "Sunil Kumar" });
   const [trainee, setTrainee] = useState([
-    {
-      name: "Aakriti Saxena",
-    },
-    {
-      name: "Ritu Yadav",
-    },
-    {
-      name: "Muskan Gupta",
-    },
+        {
+        name: "Aakriti Saxena",
+        },
+        {
+        name: "Ritu Yadav",
+        },
+        {
+            name: "Sanjay Yadav",
+        },
+        {
+        name: "Muskan Gupta",
+        },
+        {
+        name: "Shambhavi Shanker",
+        },
+        {
+            name: "Tannu mullic",
+        },
+        {
+            name: "Apporva jain",
+        },
+        {
+            name: "Ankita Mahajan",
+        },
+        {
+            name: "Prachee Singh",
+        },
   ]);
 
   return (
     <>
       <div className="DivUp1">
-        <div className="main1">Coordinator Name</div>
+        <h3 className="coord-title">Coordinator Name</h3>
         <hr />
-        <div className="Head1">
-          <h3 className="trainee-title">Trainee</h3>
-          {trainee.map((val, key) => (
-            <div className="people"> {val.name}</div>
-          ))}
-          {/* {map > div} */}
+        {/* <hr style={{ backgroundColor: "aliceblue", opacity: "0.2" }} /> */}
+        <div className="mentor"> {mentor.name}</div>
+        {/* <hr style={{ backgroundColor: "#393e46", opacity: "0.2" }} /> */}
+        <div className="trainee">
+            <h3 className="trainee-title">Trainee</h3>
+            <hr />
+            {trainee.map((val, key) => (
+                <div className="people"> {val.name} <hr style={{ backgroundColor: "#393e46", opacity: "0.2" }} /> </div>
+            ))}
         </div>
       </div>
-      {/* <div className="DivUp1">
-        <div className="container">
-          <h1 className="box-title">Co-ordinator </h1> */}
-      {/* {mentor.map(teacher => {
-                    if(teacher){
-                        return <div className="box">
-                                <b>{teacher.name}</b>
-                                </div>
-                    }else return null;
-                })}
-                <h1 className="box-title">Trainee </h1>
-                {trainee.map(student => {
-                    if(student){
-                        return <div className="box">
-                            <b>{student.name}</b>
-                            </div>
-                    }else return null;
-                })} */}
-      {/* </div>
-      </div> */}
     </>
   );
 };
