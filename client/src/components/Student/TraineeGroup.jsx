7c648c10340d0fd5894c0b7d54d148b9d5fc7f54
Shambhavi Shanker <shambhavishanker1999@gmail.com>
2023-02-27 13:20:58 +0530

    css fixed

--- client/src/components/Student/TraineeGroup.jsx
+++ client/src/components/Student/TraineeGroup.jsx
@@ -4,50 +4,62 @@ import "../../CSS/Trainee/TraineeGroup.css";
 const TraineeGroup = () => {
   const [mentor, setMentor] = useState({ name: "Sunil Kumar" });
   const [trainee, setTrainee] = useState([
-        {
-        name: "Aakriti Saxena",
-        },
-        {
-        name: "Ritu Yadav",
-        },
-        {
-            name: "Sanjay Yadav",
-        },
-        {
-        name: "Muskan Gupta",
-        },
-        {
-        name: "Shambhavi Shanker",
-        },
-        {
-            name: "Tannu mullic",
-        },
-        {
-            name: "Apporva jain",
-        },
-        {
-            name: "Ankita Mahajan",
-        },
-        {
-            name: "Prachee Singh",
-        },
+    {
+      name: "Aakriti Saxena",
+    },
+    {
+      name: "Ritu Yadav",
+    },
+    {
+      name: "Sanjay Yadav",
+    },
+    {
+      name: "Muskan Gupta",
+    },
+    {
+      name: "Shambhavi Shanker",
+    },
+    {
+      name: "Tannu mullic",
+    },
+    {
+      name: "Apporva jain",
+    },
+    {
+      name: "Ankita Mahajan",
+    },
+    {
+      name: "Prachee Singh",
+    },
   ]);
 
   return (
     <>
-      <div className="DivUp1">
-        <h3 className="coord-title">Coordinator Name</h3>
-        {/* <hr /> */}
-        <hr style={{marginLeft: "26.2%", marginRight: "25.6%"}} />
-        <div className="mentor"> {mentor.name}</div>
-        {/* <hr style={{ backgroundColor: "#393e46", opacity: "0.2" }} /> */}
-        <div className="trainee">
+      <div className="DivUp2">
+        <div className="scroll">
+          <h3 className="coord-title">Coordinator Name</h3>
+          {/* <hr /> */}
+          <hr style={{ marginLeft: "26.2%", marginRight: "25.6%" }} />
+          <div className="mentor"> {mentor.name}</div>
+          {/* <hr style={{ backgroundColor: "#393e46", opacity: "0.2" }} /> */}
+          <div className="trainee">
             <h3 className="trainee-title">Trainee</h3>
             <h3 className="strength-title">{trainee.length} trainees </h3>
-            <hr style={{marginLeft: "26.2%", marginRight: "25.6%"}} />
+            <hr style={{ marginLeft: "26.2%", marginRight: "25.6%" }} />
             {trainee.map((val, key) => (
-                <div className="people"> {val.name} <hr style={{ backgroundColor: "#393e46", opacity: "0.2", marginRight: "32.7%" }} /> </div>
+              <div className="people">
+                {" "}
+                {val.name}{" "}
+                <hr
+                  style={{
+                    backgroundColor: "#393e46",
+                    opacity: "0.2",
+                    marginRight: "32.7%",
+                  }}
+                />{" "}
+              </div>
             ))}
+          </div>
         </div>
       </div>
     </>
