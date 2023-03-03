13f9be837cc0e688f39145654b2898f848301c72
riddle17 <ritu171197@gmail.com>
2023-03-03 15:30:22 +0530

    projct css

--- client/src/components/Student/TraineeProject.jsx
+++ client/src/components/Student/TraineeProject.jsx
@@ -39,10 +39,38 @@ const TraineeProject = () => {
   return (
     <>
     <div className="DivUPro">
+
       <div className="progress">
+        <div class="circle1">
+          <h1 className="text1">SRS</h1>
+          <div class="fadedbox1">
+            <h1 class="title1 text1"> 10-MARCH-2023 </h1>
+          </div>
+        </div>
+
+        <hr style={{ width:"17.4%", marginLeft:"27.9%", marginTop:"-6%" }} ></hr>
+        
+        <div class="circle2">
+          <h1 className="text2">SDS</h1>
+          <div class="fadedbox2">
+            <h1 class="title2 text2"> 20-MARCH-2023 </h1>
+          </div>
+        </div>
+
+        <hr style={{ width:"17.4%", marginLeft:"58%", marginTop:"-6%" }} ></hr>
+        
+        <div class="circle3">
+          <h1 className="text3">Project Report</h1>
+          <div class="fadedbox3">
+            <h1 class="title3 text3"> 30-MARCH-2023 </h1>
+          </div>
+        </div>
+
       </div>
+
       <div className="project">
         <h1 className="pro"> Project Details </h1>
+        <div className="form-body-pro">
         {Projet.map((val, key) => (
           <div className="proName">
             {" "}
@@ -55,6 +83,7 @@ const TraineeProject = () => {
             />{" "}
           </div>
         ))}
+        </div>
       </div>
       <div className="document">
       </div>
