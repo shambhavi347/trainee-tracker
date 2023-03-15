938460f99940c4d9eb69ed99e227848363bbb483
Divya <divyaagoyal222@gmail.com>
2023-03-16 00:10:12 +0530

    commit

--- client/src/components/WatchTrainee.jsx
+++ client/src/components/WatchTrainee.jsx
@@ -3,7 +3,7 @@ import { getTraineeEmail } from "../service/api";
 
 const WatchTrainee = () => {
   //javascript
-  const [name, setName] = useState(["Shankar", "Aakriti", "Ritu"]);
+  const [name, setName] = useState(["Shankar", "Aakrii", "Ritu"]);
   const [email, setEmail] = useState([]);
   const handleClick = () => {
     if (name === "Shambhavi") setName("Divya");
