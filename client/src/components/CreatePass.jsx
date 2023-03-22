1c33d433ea06aacc0b92ac57ce925c5d519341d2
Shambhavi Shanker <shambhavishanker1999@gmail.com>
2023-03-22 23:20:11 +0530

    forgot password functionality

--- client/src/components/CreatePass.jsx
+++ client/src/components/CreatePass.jsx
@@ -6,22 +6,54 @@ import { useParams, useSearchParams } from "react-router-dom";
 const CreatePass = () => {
   const { userId } = useParams();
   const [changePass, setChangePass] = useState(false);
-  const [queryParameters] = useSearchParams();
   const [password, setPassword] = useState("");
-  // const [id, setId] = useState("");
-  // useEffect(() => {
-  //   setId(queryParameters.get("id"));
-  // }, []);
-  console.log(userId);
+  const [passUpper, setPassUpper] = useState("red");
+  const [passLower, setPassLower] = useState("red");
+  const [passNum, setPassNum] = useState("red");
+  const [passSpe, setPassSpe] = useState("red");
+  const [passMin, setPassMin] = useState("red");
+  const [btnDisable, setBtnDisable] = useState(true);
+  useEffect(() => {
+    var hasNumber = /\d/;
+    var format = /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+    if (password.length === 0) {
+      setPassUpper("red");
+      setPassLower("red");
+      setPassMin("red");
+      setPassNum("red");
+      setPassSpe("red");
+      setBtnDisable(true);
+    } else {
+      if (password !== password.toLowerCase()) setPassUpper("green");
+      else setPassUpper("red");
+      if (password !== password.toUpperCase()) setPassLower("green");
+      else setPassLower("red");
+      if (hasNumber.test(password)) setPassNum("green");
+      else setPassNum("red");
+      if (format.test(password)) setPassSpe("green");
+      else setPassSpe("red");
+      if (password.length > 7) setPassMin("green");
+      else setPassMin("red");
+    }
+  }, [password]);
+  useEffect(() => {
+    if (
+      passLower === "green" &&
+      passUpper === "green" &&
+      passNum === "green" &&
+      passSpe === "green" &&
+      passMin === "green"
+    )
+      setBtnDisable(false);
+    else setBtnDisable(true);
+  }, [passUpper, passLower, passMin, passNum, passSpe]);
+
   const handleChange = (e) => {
     e.preventDefault();
-    // console.log(e.target.value);
     setPassword(e.target.value);
   };
   const postData = async (e) => {
     e.preventDefault();
-
-    // const { email, password } = admin;
 
     e.preventDefault();
     const res = await fetch("/create-pass", {
@@ -41,24 +73,8 @@ const CreatePass = () => {
     if (res.status === 400) {
       window.alert(reason.error);
     } else {
-      // if (reason.message === "Admin") {
-      //   console.log("Admin login");
-      //   routeChangeAdmin();
-      // } else if (reason.message === "Institute") {
-      //   console.log("Trainee Login");
-      //   routeChangeInst();
-      // } else if (reason.message === "Trainee") {
-      //   console.log("Trainee Login");
-      //   routeChangeTrainee();
-      // } else
       console.log("Password Changed");
     }
-    // try {
-    //   const res = await axios.post("/create-pass", userId);
-    //   console.log(res);
-    // } catch (error) {
-    //   console.log(error);
-    // }
   };
   return (
     <>
@@ -80,14 +96,31 @@ const CreatePass = () => {
                 <input
                   type="text"
                   placeholder="New Password"
-                  className="pass-input"
+                  className="pass-input-change"
                   onChange={handleChange}
                 />
 
-                <br />
-                <button className="pass-btn" onClick={postData}>
-                  Change Password
-                </button>
+                {btnDisable ? (
+                  <button
+                    className="pass-btn-change-disable"
+                    disabled={btnDisable}
+                  >
+                    Change Password
+                  </button>
+                ) : (
+                  <button className="pass-btn-change" onClick={postData}>
+                    Change Password
+                  </button>
+                )}
+
+                <ul className="password-rules">
+                  <li style={{ color: passUpper }}>Uppercase letter</li>
+                  <li style={{ color: passLower }}>Lowercase letter</li>
+                  <li style={{ color: passSpe }}>Special symbol</li>
+                  <li style={{ color: passNum }}>Numeric value</li>
+                  <li style={{ color: passMin }}>Minimum of 8 letters</li>
+                </ul>
+                {/* <br /> */}
               </form>
             </>
           )}
